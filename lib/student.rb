aa26a4db35e3d681368e28d401e54e164340ad06
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 16:38:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,14 +14,14 @@ class Student
   end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
       CREATE TABLE students(
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-      ); 
+      );
       SQL
-      DB[:conn].execute(sql) 
-    end 
+      DB[:conn].execute(sql)
+    end
 
 end
