0798c20f87d756f5b1c5d6c38ab1f70b3868ad47
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 16:45:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,8 @@ class Student
         VALUES (?,?)
         SQL
         DB[:conn].execute(sql, name, grade)
-
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      end 
+    end 
 
 end
