1385c0c8e02ce83095584bbdeacacec3ed885175
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 16:50:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,4 +49,10 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
+    def self.create(name:, grade:)
+      new_student = self.new(name, grade)
+      new_student.save
+      new_student
+    end
+
 end
