b801722d3c666b0e25f3280c4cb19bd47af7dd27
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 16:58:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,11 +61,12 @@ class Student
     end
 
     def self.find_by_name(name)
-      sql = <<-SQL 
-        SELECT * 
-        FROM students 
+      sql = <<-SQL
+        SELECT *
+        FROM students
         WHERE name = ?;
-      SQL 
-        
+      SQL
+      DB[:conn].execute(sql,name)
+    end
 
 end
